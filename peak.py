cd8bce07010e34e0ccd772fdc2ae33a8b84d7ff1
Kyle1668 <kyledevinobrien1>
2017-06-27 12:13:48 -0700

    Added recursive approach

--- peak.py
+++ peak.py
@@ -1,4 +1,51 @@
 
+def get_peak_iteratively(in_nums):
+    peaks = []
+
+    for index in range(len(in_nums)):
+        if index == 1 and in_nums[index] > in_nums[index + 1]:
+            peaks.append(in_nums[index])
+        elif index == len(in_nums) and in_nums[index] > in_nums[index - 1]:
+            peaks.append(in_nums[index])
+        elif in_nums[index] > in_nums[index - 1] and in_nums[index] > in_nums[index + 1]:
+            peaks.append(in_nums[index])
+
+    return peaks
+
+
+def get_peak_recursively(in_nums):
+    current = in_nums[int((len(in_nums) / 2) - 1)]
+
+    if len(in_nums) == 1:
+        return current
+    elif current < in_nums[(int((len(in_nums) / 2) - 1)) - 1]:
+        get_peak_recursively(in_nums[:int((len(in_nums) / 2) - 1)])
+    elif current < in_nums[(int((len(in_nums) / 2) - 1)) + 1]:
+        get_peak_recursively(in_nums[int((len(in_nums) / 2) - 1):])
+    else:
+        return current
+
+
+def test_iterative_approach():
+    prompt()
+    data = get_nums()
+    peak = get_peak_iteratively(data)
+    print("\nNumbers:", end=' ')
+    print(data)
+    print("\nPeak:", end=' ')
+    print(peak)
+
+
+def test_recursive_approach():
+    prompt()
+    data = get_nums()
+    peak = get_peak_recursively(data)
+    print("\nNumbers:", end=' ')
+    print(data)
+    print("\nPeak:", end=' ')
+    print(peak)
+
+
 def prompt():
     print("\nPython Peak Finder!\n")
     print("Enter a series of positive integers.")
@@ -6,7 +53,7 @@ def prompt():
     print("__________________________________________\n")
 
 
-def getnums():
+def get_nums():
     num_list = []
     repeat = True
 
@@ -26,31 +73,9 @@ def getnums():
     return num_list
 
 
-def getpeaks(in_nums):
-    peaks = []
-
-    for index in range(len(in_nums)):
-        if index == 1 and in_nums[index] > in_nums[index + 1]:
-            peaks.append(in_nums[index])
-        elif index == len(in_nums) and in_nums[index] > in_nums[index - 1]:
-            peaks.append(in_nums[index])
-        elif in_nums[index] > in_nums[index - 1] and in_nums[index] > in_nums[index + 1]:
-            peaks.append(in_nums[index])
-
-    return peaks
-
-
 def main():
-    prompt()
-    data = getnums()
-    peaks = getpeaks(data)
-
-    print("\nNumbers:", end=' ')
-    print(data)
-    print("\nPeaks:", end=' ')
-    print(peaks)
+    test_recursive_approach()
 
 
 main()
-
 
